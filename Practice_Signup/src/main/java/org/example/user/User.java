59c2a062609d29316f5fcaa1436d3fb0ece0324f
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-06-11 22:54:42 +0900

    스프링 MVC start!

---
+++ Practice_Signup/src/main/java/org/example/user/User.java
@@ -0,0 +1,23 @@
+package org.example.user;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+
+
+public class User {
+
+    private String email;
+    private String password;
+
+    public User(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
