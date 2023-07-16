b8787c9af64048aa7c22e8b0994d6f05012786b2
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-07-16 13:31:19 +0900

    스프링 핵심원리-기본편 -복습 생성자(를 통해) 주입(injection) _관심사의 분리 인터페이스에만 의존.

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,21 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+     // 인터페이스에만 의존하기
+    private final MemberRepository memberRepository;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final DiscountPolicy discountPolicy;
+    // 인터페이스에만 의존하도록 설계와 코드를 변경했다. 실제 실행을 해보면 null pointer exception NPE가 발생한다.
+    // 이 문제를 해결하려면 누군가가 클라이언트인 `OrderServiceImpl` 에 `DiscountPolicy`의 구현 객체를 대신 생성하고 주입해주어야 한다.
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
