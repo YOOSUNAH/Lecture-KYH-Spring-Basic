923b2dac27cdaf10dd7b34c33e80a5b1672669d1
YOOSUNAH <120374640+YOOSUNAH@users.noreply.github.com>
2023-07-17 21:58:09 +0900

    Update AppConfig.java

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -19,7 +19,8 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        //return new FixDiscountPolicy(); // `AppConfig`에서 할인 정책 역할을 담당하는 구현을 `FixDiscountPolicy` -> `RatediscountPolicy`객체로 변경했다.
+        return new RateDiscountPolicy();  // 이제 할인 정책을 변경해도, 애플리케이션의 구성역할을 담당하는 AppConfig만 변경하면 된다. 클라이언트 코드인 `OrderServiceImpl`를 포함해서 사용영역의 어떤 코드도 변경할 필요가 없다.
     }
 }
 
